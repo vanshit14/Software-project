86ac4dab998f0f12dd1978c9fb06d03fcda3d683
ushmay patel <ushmay.p@ahduni.edu.in>
2023-11-07 16:35:43 +0530

    ushmay

--- Frontend/se_project_demo/src/components/Navbar.jsx
+++ Frontend/se_project_demo/src/components/Navbar.jsx
@@ -1,20 +1,51 @@
 import React from 'react';
-import { useLocation,Link } from 'react-router-dom';
+import { useLocation,Link, useNavigate } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+
+  const navigate = useNavigate();
+
+
 
   const location = useLocation();
   const isLoginRoute = location.pathname === '/';
   const isRegisterRoute = location.pathname === '/RegistrationForm';
 
   const shouldRenderNavbar = !isLoginRoute && !isRegisterRoute;
+
+
+  const signup = ()=>{
+    navigate('/RegistrationForm');
+  }
   
   return shouldRenderNavbar ? (
     <nav className="navbar">
-    <Link to="/home">Home</Link>
-    <Link to="/about">About</Link>
+      <div className='symbol'>
+        symbol
+
+      </div>
+      <div className='home'>
+      <Link to="/home">Home</Link>
+      </div>
+
+      <div className='about'>
+      <Link to="/about">About</Link>
+      </div>
+    
+    <div className='profile'>
     <Link to="/profile">Profile</Link>
+    </div>
+    
+    <div className='signin'>
+    <button >sign in</button>
+    </div>
+   
+   <div className='signup'>
+   <button onClick={signup}>sign up</button>
+   </div>
+   
+    
   </nav>
   
   ) : null;
